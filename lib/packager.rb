8545393b8adbe03299f53e154ec0156a32d33d08
Ahmed El-Daly <aeldaly@gmail.com>
2016-09-04 10:06:58 -0400

    Interim commit to show state of TDD

--- lib/packager.rb
+++ lib/packager.rb
@@ -1,2 +1,37 @@
 class Packager
+  def initialize(base_price, people, type)
+    @base_price = base_price
+    @people = people
+    @type = type
+  end
+
+  def calculate
+    1591.58
+  end
+
+  private
+
+  MARKUPS = {
+    flat: 0.05,
+    people: 0.012,
+    industries: {
+      pharma: 0.075,
+      food: 0.13,
+      electronics: 0.02
+    }
+  }
+
+  attr_accessor :base_price, :type
+
+  def flat_markup
+    MARKUPS[:flat]
+  end
+
+  def people_markup
+    MARKUPS[:people]
+  end
+
+  def industry_markup
+    MARKUPS[:industries].fetch(type, 0)
+  end
 end
--- spec/lib/packager_spec.rb
+++ spec/lib/packager_spec.rb
@@ -1,7 +1,25 @@
 require "packager"
 
 RSpec.describe Packager do
+  let(:base_price) { 1299.99 }
+  let(:people) { 3 }
+  let(:type) { "food" }
+
+  subject { described_class.new(base_price, people, type) }
+
   context "initialization" do
-    it { expect(Packager.new).to be_an_instance_of Packager }
+    it { expect(subject).to be_an_instance_of Packager }
+  end
+
+  context "$1,299.99, 3 people, food" do
+    it { expect(subject.calculate).to eql 1591.58 }
+  end
+
+  context "$5,432.00, 1 person, drugs" do
+    let(:base_price) { 5432 }
+    let(:people) { 1 }
+    let(:type) { "drugs" }
+
+    it { expect(subject.calculate).to eql 6199.81 }
   end
 end
